6783fa72954d073f417dac6df3121d45e75bd4a6
load28 <tjalsdud89@naver.com>
2024-04-22 21:27:27 +0900

    change bash

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-  PR_TITLE="change title3"
-  echo "PR title: $PR_TITLE"
-  if [[ ! $PR_TITLE =~ #[[:alnum:]]+$ ]]; then
-    echo "PR title does not meet the condition"
-    exit 1
-  fi
+PR_TITLE="change title3 #asdfasdf"
+
+if [[ ! $PR_TITLE =~ \#[[:alnum:][:punct:]]+$ ]]; then
+  echo "PR title does not meet the condition"
+  exit 1
+fi
